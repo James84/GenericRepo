8d2b57e6501c751519fb32ba366b52e7104e5925
James Runham <james.runham@reedonline.co.uk>
2017-02-17 15:30:33 +0000

    generic repository built

--- GenericRepository.DAL/BaseEFRepository.cs
+++ GenericRepository.DAL/BaseEFRepository.cs
@@ -1,8 +1,70 @@
-﻿using GenericRepository.DAL.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using GenericRepository.DAL.Interfaces;
 
 namespace GenericRepository.DAL
 {
-    public class BaseEFRepository : IBaseEFRepository
+    public abstract class BaseEFRepository<TEntity, TContext> : IBaseEFRepository<TEntity> 
+                                                                where TEntity : class 
+                                                                where TContext : DbContext, new ()
     {
+        public int Insert(TEntity entity)
+        {
+            using (var context = GetContext())
+            {
+                context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+
+                return 0;
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            using (var context = GetContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(predicate);
+            }
+        }
+
+        public ICollection<TEntity> GetAll()
+        {
+            using (var context = GetContext())
+            {
+                return context.Set<TEntity>().ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = GetContext())
+            {
+                context.Set<TEntity>().AddOrUpdate(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var context = GetContext())
+            {
+                var entity = context.Set<TEntity>().Find(id);
+
+                if (entity != null)
+                {
+                    context.Set<TEntity>().Remove(entity);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private TContext GetContext()
+        {
+            return new TContext();
+        }
     }
 }
--- GenericRepository.DAL/Interfaces/IBaseEFRepository.cs
+++ GenericRepository.DAL/Interfaces/IBaseEFRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace GenericRepository.DAL.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GenericRepository.DAL.Interfaces
 {
-    internal interface IBaseEFRepository
+    internal interface IBaseEFRepository <TEntity> where TEntity : class
     {
+        int Insert(TEntity entity);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        ICollection<TEntity> GetAll();
+        void Update(TEntity entity);
+        void Delete(int id);
     }
 }
